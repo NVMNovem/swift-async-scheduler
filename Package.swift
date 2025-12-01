f283b096eee1a9c929560fd10cdff9599ebd1734
Damian Van de Kauter <44820440+vdkdamian@users.noreply.github.com>
2025-12-01 22:38:04 +0100

    Rename SwiftAsyncScheduler to AsyncScheduler
    Updated package, source, and test names from SwiftAsyncScheduler to AsyncScheduler for consistency and clarity. Adjusted Swift tools version and refactored import/test references accordingly.

--- Package.swift
+++ Package.swift
@@ -1,26 +1,23 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "SwiftAsyncScheduler",
+    name: "swift-async-scheduler",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "SwiftAsyncScheduler",
-            targets: ["SwiftAsyncScheduler"]
+            name: "AsyncScheduler",
+            targets: ["AsyncScheduler"]
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftAsyncScheduler"
+            name: "AsyncScheduler"
         ),
         .testTarget(
-            name: "SwiftAsyncSchedulerTests",
-            dependencies: ["SwiftAsyncScheduler"]
+            name: "AsyncSchedulerTests",
+            dependencies: ["AsyncScheduler"]
         ),
     ]
 )
