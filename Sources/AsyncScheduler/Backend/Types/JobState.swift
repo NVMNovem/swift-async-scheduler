4f6412b9e96268eb48f454d0f1776453c26ebc6e
Damian Van de Kauter <44820440+vdkdamian@users.noreply.github.com>
2025-12-02 09:58:35 +0100

    Add JobState enum and refactor job state tracking
    Introduces a new JobState enum to represent job execution state and refactors AsyncScheduler to track job states per job using a dictionary. This improves handling of concurrent job execution and overrun policies.

---
+++ Sources/AsyncScheduler/Backend/Types/JobState.swift
@@ -0,0 +1,13 @@
+//
+//  JobState.swift
+//  swift-async-scheduler
+//
+//  Created by Damian Van de Kauter on 02/12/2025.
+//
+
+import Foundation
+
+internal enum JobState {
+    
+    case running
+}
